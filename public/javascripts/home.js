b29bc5e94f4cd7bf32828734e6dc08d8cc0cd911
ThisIsLuckyluck <denislucas1a@gmail.com>
2026-04-15 09:35:55 +0200

    removing message in DOM to put it in html instead

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  CheckToken();
-  document.querySelector("#welcome").innerHTML += `
-      <p>OhOhOh ! Alors comme ça tu as réussi à rentrer ? Et bah bienvenue.</p>`;
-});
 function CheckToken() {
   const token = sessionStorage.getItem("token");
   if (!token) {
